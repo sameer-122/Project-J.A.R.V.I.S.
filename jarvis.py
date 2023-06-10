23e35f1028fba27e8209620008f14a662e46e5d6
sameer <sameer.alam100@gmail.com>
2023-06-10 19:48:15 +0530

    ignoring .idea folder

--- jarvis.py
+++ jarvis.py
@@ -81,8 +81,8 @@ def fuzzmatch(desired_phrase, query):
 
 
 if __name__ == "__main__":
-    wishme()   
-                    # speak("Worship the creator, Not the creation")
+    # wishme()   
+                   # speak("Worship the creator, Not the creation")
     while True:
 
         query = takeCommand()
